9fbd83a2841e1a3211ec33ca428afdc8db293b94
PiotrMa <piotrmgrupa4@gmail.com>
2020-01-15 08:33:14 +0100

    Fixed PatternFinder.cs bug colMin not set in PatternDataResults

--- WFC_4_PatternManager/PatternFinder.cs
+++ WFC_4_PatternManager/PatternFinder.cs
@@ -1,22 +1,24 @@
-﻿using Helpers;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using UnityEngine;
-using WaveFunctionCollapse;
+using Helpers;
 
-namespace WaveFunctionCollapse
+namespace WaveFunctionCollaps
 {
     public static class PatternFinder
     {
-        
-        internal static PatternDataResults GetPatternDataFromGrid<T>(ValuesManager<T> valueManager, int patternSize, bool equalWeights)
+        public static PatternDataResults GetPatternDataFromGrid<T>(ValuesManager<T> valuesManager, int patternSize, bool equalWeights)
         {
             Dictionary<string, PatternData> patternHashcodeDictionary = new Dictionary<string, PatternData>();
             Dictionary<int, PatternData> patternIndexDictionary = new Dictionary<int, PatternData>();
-            Vector2 sizeOfGrid = valueManager.GetGridSize();
-            int patternGridSizeX = 0, patternGridSizeY = 0;
-            int rowMin = -1, colMin = -1, rowMax = -1, colMax = -1;
+            Vector2 sizeOfGrid = valuesManager.GetGridSize();
+            int patternGridSizeX = 0;
+            int patternGridSizeY = 0;
+            int rowMin = -1, colMin = -1, rowMax =-1, colMax =-1;
             if (patternSize < 3)
             {
                 patternGridSizeX = (int)sizeOfGrid.x + 3 - patternSize;
@@ -29,18 +31,23 @@ namespace WaveFunctionCollapse
                 patternGridSizeX = (int)sizeOfGrid.x + patternSize - 1;
                 patternGridSizeY = (int)sizeOfGrid.y + patternSize - 1;
                 rowMin = 1 - patternSize;
-                colMax = 1 - patternSize;
+                colMin = 1 - patternSize;
                 rowMax = (int)sizeOfGrid.y;
                 colMax = (int)sizeOfGrid.x;
             }
 
             int[][] patternIndicesGrid = MyCollectionExtension.CreateJaggedArray<int[][]>(patternGridSizeY, patternGridSizeX);
-            int totalFrequency = 0, patternIndex = 0;
+            int totalFrequency = 0;
+
+            //we loop with offset -1 / +1 to get patterns. At the same time we have to account for patten size.
+            //If pattern is of size 2 we will be reaching x+1 and y+1 to check all 4 values. Need visual aid.
+
+            int patternIndex = 0;
             for (int row = rowMin; row < rowMax; row++)
             {
                 for (int col = colMin; col < colMax; col++)
                 {
-                    int[][] gridValues = valueManager.GetPatternValuesFromGridAt(col, row, patternSize);
+                    int[][] gridValues = valuesManager.GetPatternValuesFromGridAt(col, row, patternSize);
                     string hashValue = HashCodeCalculator.CalculateHashCode(gridValues);
 
                     if (patternHashcodeDictionary.ContainsKey(hashValue) == false)
@@ -51,27 +58,29 @@ namespace WaveFunctionCollapse
                     }
                     else
                     {
+
                         if (equalWeights == false)
-                        {
                             patternIndexDictionary[patternHashcodeDictionary[hashValue].Pattern.Index].AddToFrequency();
-                        }
+
+
                     }
+                    //if (patternSize > colMin || row >= rowMin && row < rowMax-1 && col >= colMin && col < colMax-1)
+                    //{
+
+                    //    totalFrequency++;
+
+                    //}
                     totalFrequency++;
-                    if (patternSize < 3)
-                    {
+                    if (patternSize<3)
                         patternIndicesGrid[row + 1][col + 1] = patternHashcodeDictionary[hashValue].Pattern.Index;
-                    }
                     else
-                    {
-                        patternIndicesGrid[row+patternSize-1][col+patternSize-1] = patternHashcodeDictionary[hashValue].Pattern.Index;
-                    }
+                        patternIndicesGrid[row + patternSize - 1][col + patternSize - 1] = patternHashcodeDictionary[hashValue].Pattern.Index;
                 }
-                
             }
 
             CalculateRelativeFrequency(patternIndexDictionary, totalFrequency);
-            return new PatternDataResults(patternIndicesGrid, patternIndexDictionary);
 
+            return new PatternDataResults(patternIndicesGrid, patternIndexDictionary);
         }
 
         private static void CalculateRelativeFrequency(Dictionary<int, PatternData> patternIndexDictionary, int totalFrequency)
@@ -82,43 +91,60 @@ namespace WaveFunctionCollapse
             }
         }
 
-        private static void AddNewPattern(Dictionary<string, PatternData> patternHashcodeDictionary, Dictionary<int, PatternData> patternIndexDictionary, string hashValue, Pattern pattern)
+        public static Dictionary<int, PatternNeighbours> FindPossibleNeighbursForAllPatterns(IFindNeighboutStrategy patternFinder, PatternDataResults patterndataResults)
         {
-            PatternData data = new PatternData(pattern);
-            patternHashcodeDictionary.Add(hashValue, data);
-            patternIndexDictionary.Add(pattern.Index, data);
+
+            return patternFinder.FIndNeighbours(patterndataResults);
         }
 
-        internal static Dictionary<int, PatternNeighbours> FindPossibleNeighboursForAllPatterns(IFindNeighbourStrategy strategy, PatternDataResults patternFinderResult)
+        public static PatternNeighbours CheckNeighboursInEachDirection(int x, int y, PatternDataResults patterndataResults)
         {
-            return strategy.FindNeighbours(patternFinderResult);
-        }
-
-        public static PatternNeighbours CheckNeighboursInEachDirection(int x, int y, PatternDataResults patternDataResults)
-        {
-            PatternNeighbours patternNeighbours = new PatternNeighbours();
+            PatternNeighbours neighbours = new PatternNeighbours();
             foreach (Direction dir in Enum.GetValues(typeof(Direction)))
             {
-                int possiblePatternIndex = patternDataResults.GetNeighbourInDirection(x, y, dir);
-                if(possiblePatternIndex >= 0)
+                int possiblePatternIndex = patterndataResults.GetNeighbourInDirection(x, y, dir);
+                if (possiblePatternIndex >= 0)
                 {
-                    patternNeighbours.AddPatternToDictionary(dir, possiblePatternIndex);
+                    neighbours.AddPatternToDirection(dir, possiblePatternIndex);
                 }
             }
-            return patternNeighbours;
-
+            return neighbours;
         }
 
         public static void AddNeighboursToDictionary(Dictionary<int, PatternNeighbours> dictionary, int patternIndex, PatternNeighbours neighbours)
         {
             if (dictionary.ContainsKey(patternIndex) == false)
             {
+
                 dictionary.Add(patternIndex, neighbours);
+
             }
-            dictionary[patternIndex].AddNeighbour(neighbours);
+            dictionary[patternIndex].AddNeighbours(neighbours);
+
+        }
+
+        private static void AddNewPattern(Dictionary<string, PatternData> patternHashcodeDictionary, Dictionary<int, PatternData> patternIndexDictionary, string hashValue, Pattern pattern)
+        {
+
+            PatternData patternData = new PatternData(pattern);
+            patternHashcodeDictionary.Add(hashValue, patternData);
+            patternIndexDictionary.Add(pattern.Index, patternData);
+        }
+
+        public static bool AreArraysTheSame(int[][] arr1, int[][] arr2)
+        {
+            string arr1hash = HashCodeCalculator.CalculateHashCode(arr1);
+            string arr2hash = HashCodeCalculator.CalculateHashCode(arr2);
+            return arr1hash.Equals(arr2hash);
+
         }
 
 
     }
+
+
+
+
+
 }
 
