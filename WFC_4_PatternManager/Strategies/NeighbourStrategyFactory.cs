e37c49e48be6b6200bed7e11817ff471018be67b
Daynvheur <gweltaz27@hotmail.com>
2022-05-12 00:15:54 +0200

    Update NeighbourStrategyFactory.cs
    Simplification

--- WFC_4_PatternManager/Strategies/NeighbourStrategyFactory.cs
+++ WFC_4_PatternManager/Strategies/NeighbourStrategyFactory.cs
@@ -15,40 +15,23 @@ namespace WaveFunctionCollapse
             LoadTypesIFindNeighbourStrategy();
         }
 
-        private void LoadTypesIFindNeighbourStrategy()
-        {
-            strategies = new Dictionary<string, Type>();
-            Type[] typesInThisAssembly = Assembly.GetExecutingAssembly().GetTypes();
+		private void LoadTypesIFindNeighbourStrategy()
+		{
+			strategies = new Dictionary<string, Type>();
+			Type[] typesInThisAssembly = Assembly.GetExecutingAssembly().GetTypes();
 
-            foreach(var type in typesInThisAssembly)
-            {
-                if (type.GetInterface(typeof(IFindNeighbourStrategy).ToString()) != null)
-                {
-                    strategies.Add(type.Name.ToLower(), type);
-                }
-            }
-        }
+			foreach (var type in typesInThisAssembly.Where(type => type.GetInterface(typeof(IFindNeighbourStrategy).ToString()) != null))
+				strategies.Add(type.Name.ToLower(), type);
+		}
 
-        internal IFindNeighbourStrategy CreteInstance(string nameOfStrategy)
-        {
-            Type t = GetTypeToCreate(nameOfStrategy);
-            if (t == null)
-            {
-                t = GetTypeToCreate("more");
-            }
-            return Activator.CreateInstance(t) as IFindNeighbourStrategy;
-        }
+		internal IFindNeighbourStrategy CreateInstance(string nameOfStrategy)
+		{
+			Type t = GetTypeToCreate(nameOfStrategy, strategies) ?? GetTypeToCreate("more", strategies);
+			return Activator.CreateInstance(t) as IFindNeighbourStrategy;
+		}
 
-        private Type GetTypeToCreate(string nameOfStrategy)
-        {
-            foreach (var possibleStrategy in strategies)
-            {
-                if (possibleStrategy.Key.Contains(nameOfStrategy)){
-                    return possibleStrategy.Value;
-                }
-            }
-            return null;
-        }
+		private Type GetTypeToCreate(string nameOfStrategy, Dictionary<string, Type> strategies)
+			=> strategies.FirstOrDefault(possibleStrategy => possibleStrategy.Key.Contains(nameOfStrategy)).Value;
     }
 
 }
