d51586d220a77af9efcebc73f4ef41b2e2f6b2e8
Daynvheur <gweltaz27@hotmail.com>
2022-05-11 21:12:19 +0200

    Update HashCodeCalculator.cs
    More concise codelines

--- Helpers/HashCodeCalculator.cs
+++ Helpers/HashCodeCalculator.cs
@@ -1,42 +1,17 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
 
 public static class HashCodeCalculator 
 {
     //https://support.microsoft.com/da-dk/help/307020/how-to-compute-and-compare-hash-values-by-using-visual-c
-    public static string CalculateHashCode(int[][] grid)
-    {
-        byte[] tmpSource = grid.SelectMany(x => GetByteArrayFromIntArray(x)).ToArray();
+		public static string CalculateHashCode(int[][] grid)
+			=> ByteArrayToString(MD5.Create().ComputeHash(grid.SelectMany(GetByteArrayFromIntArray).ToArray()));
 
-        byte[] tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
-        return ByteArrayToString(tmpHash);
+		private static IEnumerable<byte> GetByteArrayFromIntArray(int[] tArray)
+			=> tArray.SelectMany(BitConverter.GetBytes);
 
-    }
-
-    private static byte[] GetByteArrayFromIntArray(int[] intArray)
-
-    {
-
-        byte[] data = new byte[intArray.Length * 4];
-
-        for (int i = 0; i < intArray.Length; i++)
-
-            Array.Copy(BitConverter.GetBytes(intArray[i]), 0, data, i * 4, 4);
-
-        return data;
-
-    }
-
-    private static string ByteArrayToString(byte[] arrInput)
-    {
-        int i;
-        StringBuilder sOutput = new StringBuilder(arrInput.Length);
-        for (i = 0; i < arrInput.Length; i++)
-        {
-            sOutput.Append(arrInput[i].ToString("X2"));
-        }
-        return sOutput.ToString();
-    }
+		private static string ByteArrayToString(byte[] byteArray)
+			=> string.Join(null, byteArray.Select(b => b.ToString("X2")));
 }
